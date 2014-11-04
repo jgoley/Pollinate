442b45a70992357d0fade4af54d62a72a090073d
jgoley <jgoley@gmail.com>
2014-11-03 20:00:57 -0500

    Fixes functions

--- app/js/fn.js
+++ app/js/fn.js
@@ -1,10 +1,9 @@
+// Add listenTo method
 var BaseView = function(options) {
     this.bindings = [];
     Parse.View.apply(this, [options]);
 };
-
 _.extend(BaseView.prototype, Parse.View.prototype, {
-
     listenTo: function(model, ev, callback) {
         model.bind(ev, callback, this);
         this.bindings.push({
@@ -13,33 +12,24 @@ _.extend(BaseView.prototype, Parse.View.prototype, {
             callback: callback
         });
     },
-
     stopListeningAll: function() {
         _.each(this.bindings, function(binding) {
             binding.model.unbind(binding.ev, binding.callback);
         });
         this.bindings = [];
-        console.log('stop listening');
     },
-
     dispose: function() {
-        this.stopListeningAll(); // Will unbind all events this view has bound to
-        this.unbind(); // This will unbind all listeners to events from 
-        // this view. This is probably not necessary 
-        // because this view will be garbage collected.
-        this.remove(); // Uses the default Parse.View.remove() method which
-        // removes this.el from the DOM and removes DOM events.
+        this.stopListeningAll();
+        this.unbind();
+        this.remove();
         _.invoke(this.subViews, 'dispose');
-        console.log('removing view');
     }
 
 });
-
 BaseView.extend = Parse.View.extend;
 
 
 function sendMail(fromEmail, toEmail, subject, body){
-
     $.ajax({
       type: "POST",
       url: "https://mandrillapp.com/api/1.0/messages/send.json",
@@ -61,10 +51,12 @@ function sendMail(fromEmail, toEmail, subject, body){
      })
 }
 
+
 // Round numbers
 function roundToTwo(num) {    
     return +(Math.round(num + "e+2")  + "e-2");
 }
+
 
 // Turn form data into object
 $.fn.serializeObject = function() {
@@ -79,14 +71,6 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
         return opts.fn(this);
     else
         return opts.inverse(this);
-});
-
-Handlebars.registerHelper('dateFormat', function(context, block) {
-  if (window.moment) {
-    return moment(context).format("dddd, MMMM Do YYYY"); 
-  }else{
-    return context;   //  moment plugin not available. return data as is.
-  };
 });
 
 Handlebars.registerHelper("log", function(data) {
