b6274c6668cfa8e542bb04bc2764524e187e185b
jgoley <jgoley@gmail.com>
2014-11-04 15:30:38 -0500

    adds functions

--- app/js/cloud-fn.js
+++ app/js/cloud-fn.js
@@ -1,15 +1,26 @@
-function sendMail(params){
+function sendMail(params) {
 
-	 Parse.Cloud.run('sendEmail', {
-	     message: 'A farmer is requesting some of your hives',
-	     subject: 'New Request for bees'
-	 }, {
-	     success: function(result) {
-	         console.log(result)
-	     },
-	     error: function(error) {
-	         console.log(error);
-	     }
-	 });
+    Parse.Cloud.run('sendEmail', {
+        message: 'A farmer is requesting some of your hives',
+        subject: 'New Request for bees'
+    }, {
+        success: function(result) {
+            console.log(result)
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 
+}
+
+function() {
+    Parse.Cloud.run('queryBeekeepers', {}, {
+        success: function(result) {
+            console.log("Result from cloud fn", result);
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    });
 }
