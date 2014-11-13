41369e99a5388ef7b7dd69cd4ba496151bf4b776
jgoley <jgoley@gmail.com>
2014-11-13 17:35:31 -0500

    Adds use strict and wraps

--- app/js/views/editAccount.js
+++ app/js/views/editAccount.js
@@ -1,76 +1,81 @@
-Bees.Views.EditAccountView = BaseView.extend({
-    tagName: 'form',
-    className: 'edit-account',
-    subViews: [],
-    template: Bees.templates.account.edit,
-    events: {
-        'submit': 'saveUser',
-        'change .userType': 'addUserInfo',
-        'change .image': 'getImage'
-    },
+(function(){
 
-    initialize: function(opts) {
-        var options = _.defaults({}, opts, {
-            $container: opts.$container,
-        });
-        options.$container.html(this.el);
-        this.render();
-    },
+    'use strict';
+    Bees.Views.EditAccountView = BaseView.extend({
+        tagName: 'form',
+        className: 'edit-account',
+        subViews: [],
+        template: Bees.templates.account.edit,
+        events: {
+            'submit': 'saveUser',
+            'change .userType': 'addUserInfo',
+            'change .image': 'getImage'
+        },
 
-    render: function() {
-        this.$el.prepend(this.template({
-            user: this.model.toJSON()
-        }));
-        this.$el.parsley();
-        $("[name=state]").val(this.model.get("state"));
-        $("[name=userType]").val(this.model.get("userType"));
-    },
-
-    saveUser: function(e) {
-        e.preventDefault();
-        var user = this.model;
-        var credentials = this.$el.serializeObject();
-        user.set(credentials);
-        user.set('costPerHive', +credentials.costPerHive);
-        user.set('maxDistFree', +credentials.maxDistFree);
-        user.set('costPerMile', +credentials.costPerMile);
-        // user.set('hivesAvailable', user.get('hivesAvailable') + ( user.get('hivesTotal') + credentials.hivesTotal ) );
-        user.set('hivesTotal', +credentials.hivesTotal);
-        user.set('zipCode', +credentials.zipCode);
-        user.set('geoRangeRadius', +credentials.geoRangeRadius);
-        user.save(null, {
-            success: function(a, b) {
-                saveLocation();
-            },
-            error: function(a, err) {
-                console.log(err)
-            }
-        });
-        BeesApp.navigate('/', {
-            trigger: true
-        });
-        this.dispose();
-    },
-
-    getImage: function(e) {
-        var that = this;
-        var image = $(e.target)[0].files[0];
-        var file = new Parse.File(image.name, image);
-        file.save()
-            .then(function() {
-                $('.image').attr('disabled', false);
-                // $('.loading').remove();
-                that.model.set('image', file.url());
+        initialize: function(opts) {
+            var options = _.defaults({}, opts, {
+                $container: opts.$container,
             });
-    },
+            options.$container.html(this.el);
+            this.render();
+        },
 
-    addUserInfo: function(e){
-        _.invoke(this.subViews, 'dispose');
-        var userType = $(e.target).val();
-        this.subViews.push(
-            new Bees.Views.UserTypeFormFields({
-            $container: $('.userType-info'),
-            userType: userType,
-        }));
-    },
-});
+        render: function() {
+            this.$el.prepend(this.template({
+                user: this.model.toJSON()
+            }));
+            this.$el.parsley();
+            $("[name=state]").val(this.model.get("state"));
+            $("[name=userType]").val(this.model.get("userType"));
+        },
+
+        saveUser: function(e) {
+            e.preventDefault();
+            var user = this.model;
+            var credentials = this.$el.serializeObject();
+            user.set(credentials);
+            user.set('costPerHive', +credentials.costPerHive);
+            user.set('maxDistFree', +credentials.maxDistFree);
+            user.set('costPerMile', +credentials.costPerMile);
+            // user.set('hivesAvailable', user.get('hivesAvailable') + ( user.get('hivesTotal') + credentials.hivesTotal ) );
+            user.set('hivesTotal', +credentials.hivesTotal);
+            user.set('zipCode', +credentials.zipCode);
+            user.set('geoRangeRadius', +credentials.geoRangeRadius);
+            user.save(null, {
+                success: function(a, b) {
+                    saveLocation();
+                },
+                error: function(a, err) {
+                    console.log(err)
+                }
+            });
+            BeesApp.navigate('/', {
+                trigger: true
+            });
+            this.dispose();
+        },
+
+        getImage: function(e) {
+            var that = this;
+            var image = $(e.target)[0].files[0];
+            var file = new Parse.File(image.name, image);
+            file.save()
+                .then(function() {
+                    $('.image').attr('disabled', false);
+                    // $('.loading').remove();
+                    that.model.set('image', file.url());
+                });
+        },
+
+        addUserInfo: function(e){
+            _.invoke(this.subViews, 'dispose');
+            var userType = $(e.target).val();
+            this.subViews.push(
+                new Bees.Views.UserTypeFormFields({
+                $container: $('.userType-info'),
+                userType: userType,
+            }));
+        },
+    });
+
+})();
--- app/js/views/login.js
+++ app/js/views/login.js
@@ -1,41 +1,48 @@
-Bees.Views.LoginView = BaseView.extend({
-    tagName: 'form',
-    className: 'user-login',
-    template: Bees.templates.login,
 
-    events: {
-        'submit': 'loginUser'
-    },
+(function(){
 
-    initialize: function(opts) {
-        var options = _.defaults({}, opts, {
-            $container: opts.$container,
-            session: opts.session
-        });
-        options.$container.html(this.el);
-        this.render();
-    },
+    'use strict';
 
-    render: function() {    
-        this.$el.prepend(this.template());
-        this.$el.parsley();
-    },
+    Bees.Views.LoginView = BaseView.extend({
+        tagName: 'form',
+        className: 'user-login',
+        template: Bees.templates.login,
 
-    loginUser: function(e) {
-        e.preventDefault();
-        var that = this;
-        var credentials = this.$el.serializeObject();
-        Parse.User.logIn(credentials.userName, credentials.pass, {
-            success: function(user) {
-                Bees.Session.set('user', user)
-                BeesApp.navigate('/', {
-                    trigger: true
-                });
-                // that.dispose();
-            },
-            error: function(user, error) {
-                alert(error.message);
-            }
-        });
-    }
-});
+        events: {
+            'submit': 'loginUser'
+        },
+
+        initialize: function(opts) {
+            var options = _.defaults({}, opts, {
+                $container: opts.$container,
+                session: opts.session
+            });
+            options.$container.html(this.el);
+            this.render();
+        },
+
+        render: function() {    
+            this.$el.prepend(this.template());
+            this.$el.parsley();
+        },
+
+        loginUser: function(e) {
+            e.preventDefault();
+            var that = this;
+            var credentials = this.$el.serializeObject();
+            Parse.User.logIn(credentials.userName, credentials.pass, {
+                success: function(user) {
+                    Bees.Session.set('user', user)
+                    BeesApp.navigate('/', {
+                        trigger: true
+                    });
+                    // that.dispose();
+                },
+                error: function(user, error) {
+                    alert(error.message);
+                }
+            });
+        }
+    });
+
+})();
